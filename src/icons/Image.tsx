2a5d7188f4a32280aa4e7206bbfd3a6ff22359a1
daishi <daishi@axlight.com>
2021-02-14 10:14:25 +0900

    update icons

---
+++ src/icons/Image.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"; // for expo
+import { Path } from "react-native-svg";
+
+const Image: React.FC = () => (
+  <>
+    <Path d="M0 0h24v24H0z" fill="none" />
+    <Path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z" />
+  </>
+);
+
+export default Image;
--- src/icons/Redo.tsx
+++ src/icons/Redo.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"; // for expo
 import { Path } from "react-native-svg";
 
-const Delete: React.FC = () => (
+const Redo: React.FC = () => (
   <>
     <Path d="M0 0h24v24H0z" fill="none" />
     <Path d="M18.4 10.6C16.55 8.99 14.15 8 11.5 8c-4.65 0-8.58 3.03-9.96 7.22L3.9 16c1.05-3.19 4.05-5.5 7.6-5.5 1.95 0 3.73.72 5.12 1.88L13 16h9V7l-3.6 3.6z" />
   </>
 );
 
-export default Delete;
+export default Redo;
